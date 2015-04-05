8a5c2c50f639a176d1ee3a109fb767d869496a5d
dpuyosa <d.puyosa@gmail.com>
2015-04-05 17:47:11 +0200

    First Commit

---
+++ arduino_mpu.cpp
@@ -0,0 +1,18 @@
+#include "arduino_mpu.h"
+#include <I2Cdev.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	int i2c_write(unsigned char slave_addr, unsigned char reg_addr,unsigned char length, unsigned char const *data) {
+		return !I2Cdev::writeBytes(slave_addr,reg_addr,length,(uint8_t *)data);
+	}
+
+	int i2c_read(unsigned char slave_addr, unsigned char reg_addr,unsigned char length, unsigned char *data) {
+		return !I2Cdev::readBytes(slave_addr,reg_addr,length,(uint8_t *)data);
+	}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ arduino_mpu.h
@@ -0,0 +1,47 @@
+ /* The following functions must be defined for this platform:
+ * i2c_write(unsigned char slave_addr, unsigned char reg_addr,
+ *      unsigned char length, unsigned char const *data)
+ * i2c_read(unsigned char slave_addr, unsigned char reg_addr,
+ *      unsigned char length, unsigned char *data)
+ * delay_ms(unsigned long num_ms)
+ * get_ms(unsigned long *count)
+ * reg_int_cb(void (*cb)(void), unsigned char port, unsigned char interrupt)
+ * labs(long x)
+ * fabsf(float x)
+ * min(int a, int b)
+ */
+#ifndef __ARDUINO_MPU__
+#define __ARDUINO_MPU__
+ 
+#define MPU6050
+//#define MPU_EXTRAS //Enable extras: tap, shake, pedometer and android orient
+#include <Arduino.h>
+
+#define delay_ms delay
+#define log_i(...) do {} while (0)
+#define log_e(...) do {} while (0)
+#define min(a,b) ((a<b) ? a : b)
+#define labs(x) ((x>0) ? x : -x)
+#define fabsf(x) labs(x)
+
+static inline void get_ms(unsigned long *count){
+	*count = millis();
+}
+
+static inline int reg_int_cb(struct int_param_s *int_param){
+	//TODO: Setup arduino interrupt
+}
+ 
+ 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	int i2c_write(unsigned char slave_addr, unsigned char reg_addr,unsigned char length, unsigned char const *data);
+	int i2c_read(unsigned char slave_addr, unsigned char reg_addr,unsigned char length, unsigned char *data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __ARDUINO_MPU__
