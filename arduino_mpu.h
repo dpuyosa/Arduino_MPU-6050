3861a9bdab668e0e481cb6a55dd04e17674a80f1
dpuyosa <d.puyosa@gmail.com>
2016-02-10 03:38:02 +0100

    Revert "Add Interrup Setup"
    This reverts commit 1f73f582359c2fa54320c38a823ff009b49fb093.

--- arduino_mpu.h
+++ arduino_mpu.h
@@ -20,29 +20,25 @@
 #define delay_ms delay
 #define log_i(...) do {} while (0)
 #define log_e(...) do {} while (0)
-//#define min(a,b) ((a<b) ? a : b)
-//#define labs(x) ((x>0) ? x : -x)
-//#define fabsf(x) labs(x)
-
-struct int_param_s {
-    uint8_t pin;
-    void (*cb)(void);
-};
+#define min(a,b) ((a<b) ? a : b)
+#define labs(x) ((x>0) ? x : -x)
+#define fabsf(x) labs(x)
 
 static inline void get_ms(unsigned long *count){
 	*count = millis();
 }
 
-static inline void reg_int_cb(struct int_param_s *int_param){
-	attachInterrupt(digitalPinToInterrupt(int_param->pin), int_param->cb, RISING);
+static inline int reg_int_cb(struct int_param_s *int_param){
+	//TODO: Setup arduino interrupt
 }
+ 
  
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-	int8_t i2c_write(unsigned char slave_addr, unsigned char reg_addr,unsigned char length, unsigned char const *data);
-	int8_t i2c_read(unsigned char slave_addr, unsigned char reg_addr,unsigned char length, unsigned char *data);
+	int i2c_write(unsigned char slave_addr, unsigned char reg_addr,unsigned char length, unsigned char const *data);
+	int i2c_read(unsigned char slave_addr, unsigned char reg_addr,unsigned char length, unsigned char *data);
 
 #ifdef __cplusplus
 }
